bb7c3615cb03e14a467bea6fb7bbfc64f677b356
sai-chaitanya <saichaitanya2512@gmail.com>
2022-10-27 14:29:21 +0530

    first commit

---
+++ quiz.js
@@ -0,0 +1,446 @@
+var pol = {
+
+    allquestions: [{
+
+        question: "WHAT IS HTML ?",
+        id: "id1",
+        answer: "HYPER TEXT MARK UP LANGUANGE",
+        options: ["HYPER TEXT MARK UP LANGUAGE", "HYDRO TEXT MARK UP LANGUAGE", "HYDRABAD TEXT MARK UP LANGUAGE", "HYPER TEXT MAKE UP LANGUAGE"]
+    },
+
+    {
+        question: "WHAT IS CSS ?",
+        id: "id2",
+        answer: "CASCADING STYLE SHEETS",
+        options: ["CAKE STYLE SHEETS", "CASCADING STYLE SHEETS", "COKE STYEL SHEETS", "CAMERA STYLE SHEETS"]
+
+    },
+
+    {
+        question: "WHAT IS CPP ?",
+        id: "id3",
+        answer: "C PLUS PLUS",
+        options: ["CAKE PIG PIG", "C PIG PEN", "C PLUS PLUS", "C PEN PEN"]
+
+    },
+
+    {
+        question: "WHO INVENTED HTML  ?",
+        id: "id4",
+        answer: "TIM BERNERS LEE",
+        options: ["ME ", "YOU ", "HIM", "TIM BERNERS LEE"]
+
+    },
+
+    {
+        question: "WHO INVENTED CSS  ?",
+        id: "id5",
+        answer: "HAKON WIUM LEE",
+        options: ["PEGION", "YAMMUDU", "HAKON WIUM LEE", "TIM BERNERS LEE"]
+
+    },
+
+    {
+        question: "WHO INVENTED C  ?",
+        id: "id6",
+        answer: "DENIES RICHIE",
+        options: ["GOD", "DENIES RICHIE", "HAKON WIUM LEE", "TIM BERNERS LEE"]
+
+    },
+
+    {
+        question: "WHO INVENTED JAVASCRIPT  ?",
+        id: "id7",
+        answer: "BERNERS EICH",
+        options: ["BERNERS EICH", "DENIES RICHIE", "HAKON WIUM LEE", "TIM BERNERS LEE"]
+
+    },
+    {
+        question: "JAVASCRIPT WAS INVENTED IN  ?",
+        id: "id8",
+        answer: "NETSCAPES",
+        options: ["NETSCAPES", "CLIQ TICK", "GOPALAPATNAM", "RELLI VEEDHI"]
+
+    },
+    {
+        question: "WHO IS BEST FACULTY IN CODING SCHOOL ?",
+        id: "id9",
+        answer: "MANI KANTA SIR",
+        options: ["IDDU", "ADDU", "YEVADU", "MANI KANTA SIR"]
+
+    },
+    {
+        question: "WHO IS HANDSOME GUY IN BELOW OPTIONS ?",
+        id: "id10",
+        answer: "MANI KANTA SIR",
+        options: ["MAHESH BABU", "PRABHAS", "RAM CHARAN", "MANI KANTA SIR "]
+
+    },
+    {
+        question: "WHAT IS VALUE OF 2+3",
+        id: "id11",
+        answer: 5,
+        options: [2, 1, 3, 5]
+
+    },
+    {
+        question: "WHAT IS VALUE OF 3+3",
+        id: "id12",
+        answer: 6,
+        options: [1, 2, 3, 6]
+
+    },
+    {
+        question: "WHAT IS SPELLING OF MANI KANTA SIR",
+        id: "id13",
+        answer: "MANI KANTA SIR",
+        options: ["MANIPURAM", "MALKAPURAM", "MARRIPALEM", "MANI KANTA SIR"]
+
+    },
+    {
+        question: "WHICH DEVICE HAS SCREEN AND KEYBOARD ",
+        id: "id14",
+        answer: "LAPTOP",
+        options: ["LAPTOP", "SMART PHONE", "NOKIA PHONE", "IPHONE"]
+
+    },
+    {
+        question: "WHAT IS YOUR NAME ",
+        id: "id15",
+        answer: "NONE OF THE ABOVE",
+        options: ["ME", "MYSELF", "YOU", "NONE OF THE ABOVE"]
+
+    },
+    {
+        question: "WHAT IS THE CORRECT FORMAT OF HYPER TEXT MARK UP LANGUAGE",
+        id: "id16",
+        answer: "HTML",
+        options: ["LMHT", "HTML", "MLTH", "TMLH"]
+
+    },
+    {
+        question: "FROM WHICH SIDE DOES CEILING FAN ROTATES ",
+        id: "id17",
+        answer: "ANTI CLOCKWISE",
+        options: ["CLOCKWISE", "ANTI CLOCKWISE", "UPWARD", "DOWNWARD"]
+
+    },
+    {
+        question: "WHAT IS 60+90",
+        id: "id18",
+        answer: 150,
+        options: [110, 100, 120, 150]
+
+    },
+    {
+        question: "HOW MANY LETTERS ARE THERE IN BOOK",
+        id: "id19",
+        answer: 4,
+        options: [1, 0, 20, 4]
+
+    },
+    {
+        question: "IN WHICH CODING SCHOOL IS THERE",
+        id: "id20",
+        answer: "2ND FLOOR",
+        options: ["1ST FLOOR", "2ND FLOOR", "3RD FLOOR", "4TH FLOOR"]
+
+    },
+    {
+        question: "who invented apple",
+        id: "id21",
+        answer: "steve jobs",
+        options: ["steve jobs", "me", "you", "him"]
+
+    },
+    {
+        question: "who invented google",
+        id: "id22",
+        answer: "larry page",
+        options: ["larry page", "steve jobs", "pasi reddy", "apparao"]
+
+    },
+    {
+        question: "who is ceo of google",
+        id: "id23",
+        answer: "sundar pichai",
+        options: ["sundar pichai", "vikrant rona", "pasi reddy", "apparao"]
+
+    },
+    {
+        question: "ronaldo belogs to",
+        id: "id24",
+        answer: "portugal",
+        options: ["portugal", "spain", "india", "swizerland"]
+
+    },
+    {
+        question: "ronaldo belogs to",
+        id: "id24",
+        answer: "portugal",
+        options: ["portugal", "spain", "india", "swizerland"]
+
+    },
+    {
+
+        id: "id25",
+        question: "who is making web standards?",
+        answer: "the world wide web consortium",
+        options: ["the world wide web consortium", "mozilla", "firefox", "microsoft"]
+    },
+    {
+        id: "id26",
+        question: "Choose the correct HTML element for the largest heading",
+        answer: "h1",
+        options: ["h1", "h5", "h3", "h4"]
+    },
+    {
+        id: "id27",
+        question: "Choose the correct HTML element to define important text",
+        answer: "important",
+        options: ["strong", "important", "strike", "i"]
+    },
+    {
+        id: "id28",
+        question: "Which HTML element defines the title of a document?",
+        answer: "title",
+        options: ["title", "head", "body", "meta"]
+    },
+    {
+        id: "id29",
+        question: "In html, which attribute is used to specify that an input field must be filled out?",
+        answer: "required",
+        options: ["placeholder", "validate", "form validate", "required"]
+    },
+    {
+        id: "id30",
+        question: "Which input type defines a slider control?",
+        answer: "range",
+        options: ["range", "slider", "controls", "audio"]
+    },
+    {
+
+        id: "id31",
+        question: "bootstrap5 is released in which year ?",
+        answer: 2021,
+        options: [1995, 1990, 2001, 2021]
+    },
+    {
+
+        id: "id32",
+        question: "cdn full form ?",
+        answer: "content delivery network",
+        options: ["content delivery network", "consortium delivery network", "content data net", "create data net"]
+    },
+    {
+
+        id: "id33",
+        question: "sql full form ?",
+        answer: "structured query language",
+        options: ["structured query language", "structure quote language", "struct query language", "struct quote language"]
+    },
+    {
+
+        id: "id34",
+        question: "how do you group selectors?",
+        answer: "seperate each selector with comma",
+        options: ["seperate each selector with plus", "seperate each selector with spaces", "seperate each selector with comma", "nothing"]
+    },
+    {
+
+        id: "id35",
+        question: "the bootstrap grid system is based on how many columns?",
+        answer: "12",
+        options: ["6", "12", "8", "4"]
+    },
+    {
+
+        id: "id36",
+        question: "which class shapes an image to a circle in bootstrap?",
+        answer: "rounded circle",
+        options: ["rounded-cicle", "circle", "img round", "img"]
+    },
+    {
+
+        id: "id37",
+        question: "Which class is used to create a big box for calling extra attention in bootstrap?",
+        answer: "jumbotron",
+        options: ["container", "container-fluid", "jumbotron", "container responsive"]
+    },
+    {
+
+        id: "id38",
+        question: "which class is used to create a basic pagination in bootstrap?",
+        answer: "pagination",
+        options: ["pagination", "pages", "navigation", "pages"]
+    },
+    {
+
+        id: "id39",
+        question: "in which year let and const keywords were added to javascript ?",
+        answer: 1995,
+        options: [1999, 1997, 1980, 2010]
+    },
+    {
+
+        id: "id40",
+        question: "the let keyword was introduced in year ?",
+        answer: 2015,
+        options: [2011, 2018, 2009, 2015]
+    },
+    {
+
+        id: "id41",
+        question: "cpp full form ?",
+        answer: "c preprocesscor",
+        options: ["c preprocesscor", "c process", "c prepose", "c preprocess"]
+    },
+    {
+
+        id: "id42",
+        question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+        answer: "alt",
+        options: ["src", "href", "alt", "none"]
+    },
+    {
+
+        id: "id43",
+        question: "graphics defined by svg is in which format?",
+        answer: "xml",
+        options: ["html", "xml", "css", "cpp"]
+    },
+    {
+
+        id: "id44",
+        question: "the html canvas element is used to:",
+        answer: "draw graphics",
+        options: ["draw graphics", "drag objects", "manipulate data", "display data"]
+    },
+    {
+
+        id: "id45",
+        question: "which html tag is used to define an internal style sheet?",
+        answer: "style",
+        options: ["header", "script", "style", "footer"]
+    },
+    {
+
+        id: "id46",
+        question: "which html attribute is used to define inline styles?",
+        answer: "style",
+        options: ["font", "style", "styles", "none"]
+    },
+    {
+
+        id: "id47",
+        question: "which css property is used to change the text color of an element?",
+        answer: "color",
+        options: ["text color", "color", "text", "none"]
+    },
+    {
+
+        id: "id48",
+        question: "which property is used to change the font of an element?",
+        answer: "font family",
+        options: ["font weight", "font family", "font", "font style"]
+    },
+    {
+
+        id: "id49",
+        question: "which property is used to change the font color?",
+        answer: "color",
+        options: ["font color", "font colour", "colour", "color"]
+    },
+    {
+        id: "id50",
+        question: "what is the full form of DS ?",
+        answer: "Data Structures",
+        options: ["Data Structures", "Data Storage", "Data Symbol", "Data Storage"]
+
+    }
+    ]
+
+}
+
+
+
+
+let count = 0;
+let answers = [];
+let reg = 5;
+
+for (let i = 0; i < 5; i++) {
+
+
+    var rrr = Math.floor(Math.random() * 50);
+
+    var s = pol.allquestions[rrr];
+
+    [a, b, c, d] = s.options;
+
+    answers.unshift(s.answer);
+
+    let html = `
+    <div class="questions">${reg}) ${s.question}</div>
+    <div class="alloptions">
+    <input class="options" name="radio${count}" type="radio"> <span class="ans">${a}</span>
+    <input class="options" name="radio${count}" type="radio"> <span class="ans">${b}</span>
+    <input class="options" name="radio${count}" type="radio"> <span class="ans">${c}</span>
+    <input class="options" name="radio${count}" type="radio"> <span class="ans">${d}</span>
+    </div> `;
+
+    count++;
+    reg--;
+
+    document.querySelector('#sun').insertAdjacentHTML('afterbegin', html);
+
+}
+
+let ans = 0;
+let result = 0;
+
+
+function submit() {
+    for (let i = 0; i < document.querySelectorAll('.options').length; i++) {
+        let all = document.querySelectorAll('.options')[i].checked;
+        if (all == true) {
+            console.log(document.querySelectorAll('.ans')[i].innerHTML, answers[ans])
+            if (document.querySelectorAll('.ans')[i].innerHTML == answers[ans]) {
+
+                console.log('right');
+                result++;
+            }
+            else {
+
+                console.log("wrong");
+            }
+            ans++;
+        }
+        
+    }
+    document.querySelector('#sun').style.display = "none";
+    document.querySelector('#score').innerHTML = `${result} /5 `;
+    document.querySelector('#res').style.display = "block";
+    document.querySelector('#submit').style.display = "none"
+
+    if (result >= 3) {
+        document.getElementById("img").style.display = "block";
+    }
+    else {
+       document.getElementById("img").src = "./fail.gif";
+        document.getElementById("img").style.display = "block";
+       document.getElementById("img").style.width='500px' 
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
